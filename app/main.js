988833a161653b9ecd8b45b9a907e743d9e13564
Christian Dreier <flipper.x3r@googlemail.com>
2023-05-29 22:42:27 +0200

    Refactoring
    - Remove comments originated from the fiddle
    - Make the "Content-Security-Policy" stricter
    - Other minor changes

--- app/main.js
+++ app/main.js
@@ -1,44 +1,33 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron")
 const path = require("path")
 
+const electron = require("electron")
+
+const WINDOW_WIDTH_DEFAULT = 1024
+const WINDOW_HEIGHT_DEFAULT = 768
+
 function createWindow() {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+    const mainWindow = new electron.BrowserWindow({
+        width: WINDOW_WIDTH_DEFAULT,
+        height: WINDOW_HEIGHT_DEFAULT,
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
         },
     })
-
-    // and load the index.html of the app.
-    mainWindow.loadFile("index.html")
     mainWindow.loadFile(path.join(__dirname, "index.html"))
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+electron.app.whenReady().then(() => {
     createWindow()
 
-    app.on("activate", function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    electron.app.on("activate", () => {
+        if (electron.BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") app.quit()
+electron.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        electron.app.quit()
+    }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
