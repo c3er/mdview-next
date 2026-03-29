cbaf020271eff4cc36064f496f715d369d37afef
Christian Dreier <flipper.x3r@googlemail.com>
2026-03-29 05:37:51 +0200

    Add additional log messages for IPC handling

--- app/lib/ipcMainIntern.js
+++ app/lib/ipcMainIntern.js
@@ -1,8 +1,11 @@
+const log = require("./logMain")
+
 let electron
 
 const _preInitIpcListeners = []
 
 function listen(messageId, callback) {
+    log.debug(`Listening to message "${messageId}", using callback:`, callback)
     if (electron) {
         electron.ipcMain.on(messageId, (event, ...args) => callback(event.sender.id, ...args))
     } else {
@@ -27,6 +30,14 @@ exports.init = electronMock => {
 exports.listen = listen
 
 exports.handle = (message, callback) =>
-    electron.ipcMain.handle(message, (event, ...args) => callback(event.sender.id, ...args))
+    electron.ipcMain.handle(message, (event, ...args) => {
+        const senderId = event.sender.id
+        log.debug(`Handling message "${message}" for window ${senderId}; callback:`, callback)
+        return callback(senderId, ...args)
+    })
 
-exports.send = (browserWindow, message, ...args) => browserWindow.webContents.send(message, ...args)
+exports.send = (browserWindow, message, ...args) => {
+    const webContents = browserWindow.webContents
+    log.debug(`Sending message "${message}" to window with ID ${webContents.id}; arguments:`, args)
+    webContents.send(message, ...args)
+}
