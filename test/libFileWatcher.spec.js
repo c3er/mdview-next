3abe96decb483274d22eb5938fca99dc900ee438
Christian Dreier <flipper.x3r@googlemail.com>
2026-04-04 01:26:43 +0200

    Change expected file path in file watcher tests

--- test/libFileWatcher.spec.js
+++ test/libFileWatcher.spec.js
@@ -8,6 +8,8 @@ describe("File watcher", () => {
         const fileWatcher = require("../app/lib/fileWatcherRenderer")
         const ipc = require("../app/lib/ipcRenderer")
 
+        const DOCUMENT_PATH = "path/to/document"
+
         let documentRendering
 
         beforeEach(async () => {
@@ -17,32 +19,30 @@ describe("File watcher", () => {
             lib.registerElectronLogIpc()
             mocking.ipc.register.rendererInvoke(
                 ipc.messages.intern.fetchDocumentPath,
-                () => lib.DEFAULT_DOCUMENT_PATH,
+                () => DOCUMENT_PATH,
             )
             mocking.ipc.register.mainOn(ipc.messages.intern.watchFile, (_, filePath) =>
-                assert.strictEqual(filePath, lib.DEFAULT_DOCUMENT_PATH),
+                assert.strictEqual(filePath, DOCUMENT_PATH),
             )
 
             documentRendering = mocking.createDocumentRendering()
             documentRendering.render = documentPath =>
-                assert.strictEqual(documentPath, lib.DEFAULT_DOCUMENT_PATH)
+                assert.strictEqual(documentPath, DOCUMENT_PATH)
 
             await fileWatcher.init(documentRendering)
         })
 
         it("has a document path", () => {
-            assert.strictEqual(fileWatcher.documentPath(), lib.DEFAULT_DOCUMENT_PATH)
+            assert.strictEqual(fileWatcher.documentPath(), DOCUMENT_PATH)
         })
 
         it("dispatches file changes", () => {
             let dispatchCallCount = 0
             documentRendering.render = documentPath => {
-                assert.strictEqual(documentPath, lib.DEFAULT_DOCUMENT_PATH)
+                assert.strictEqual(documentPath, DOCUMENT_PATH)
                 dispatchCallCount++
             }
-            mocking.ipc.sendToRenderer(ipc.messages.intern.filesChanged, {}, [
-                lib.DEFAULT_DOCUMENT_PATH,
-            ])
+            mocking.ipc.sendToRenderer(ipc.messages.intern.filesChanged, {}, [DOCUMENT_PATH])
             assert.strictEqual(dispatchCallCount, 1)
         })
     })
